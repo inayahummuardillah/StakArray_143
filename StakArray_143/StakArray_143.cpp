77f3bbcaa41ee3ec5f38f962550fb5edda952173
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-06-15 19:15:31 +0630

    ini menggunakan for untuk melakukannya

--- StakArray_143/StakArray_143.cpp
+++ StakArray_143/StakArray_143.cpp
@@ -49,5 +49,10 @@ public:
 		if (empty()) {
 			cout << "\nStack is empty." << endl;
 		}
+		else {
+			for (int tmp = 0; tmp <= top; tmp++) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
 	}
 };
