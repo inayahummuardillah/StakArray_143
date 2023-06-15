e772c9d21fd998a254b025752cddc49356c8f140
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-06-15 18:52:40 +0630

    top ++ menambahkan index array lalu ada "stack_array[top]; data dimasukkan kedalam index itu

--- StakArray_143/StakArray_143.cpp
+++ StakArray_143/StakArray_143.cpp
@@ -21,5 +21,12 @@ public:
 			cout << "Number of data exceeds the limit." << endl;
 			return;
 		}
+
+		top++; //step 2
+
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << "ditambahkan(pushed)" << endl;
+
 	}
 };
